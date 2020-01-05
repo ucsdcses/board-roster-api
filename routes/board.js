6206b7801606de2cf3e7a818018e919aaf510116
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2020-01-04 22:08:49 -0800

    created board api

--- routes/board.js
+++ routes/board.js
@@ -1,9 +1,111 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+const readline = require('readline');
+const { google } = require('googleapis');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({users: [{name: 'Timmy'}]});
+router.get('/', function (req, res, next) {
+  // Load client secrets from a local file.
+  fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Google Sheets API.
+    authorize(JSON.parse(content), listMajors, res);
+  });
 });
 
 module.exports = router;
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback, res) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback, res);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client, res);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback, res) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error while trying to retrieve access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client, res);
+    });
+  });
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+function listMajors(auth, response) {
+  const sheets = google.sheets({ version: 'v4', auth });
+
+  // get the current date
+  let today = new Date();
+  let month = today.getMonth() + 1;
+  let year = today.getFullYear();
+  let prevYear;
+  if (month < 10) {
+    prevYear = year - 1;
+  } else {
+    prevYear = year;
+    year = prevYear + 1;
+  }
+
+  sheets.spreadsheets.values.get({
+    spreadsheetId: '1Lkq_3bH0Xxmr_Z2z_9W9htGdTRr39K9KRQTwX3fXoeI',
+    range: `${prevYear}-${year}!A2:C`,
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const rows = res.data.values;
+    if (rows.length) {
+      console.log('Name, Major:');
+      // Print columns A and C, which correspond to indices 0 and 2.
+      response.send(rows);
+    } else {
+      console.log('No data found.');
+    }
+  });
+}
